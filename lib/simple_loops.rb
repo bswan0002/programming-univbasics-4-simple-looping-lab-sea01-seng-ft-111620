ae8e04eba840c65c12c8980e9a8f79047674413f
Ben Swanson <bswan0002@gmail.com>
2020-10-12 21:49:57 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,3 +2,14 @@ def loop_message_five_times (msg)
   5.times { puts msg }
 end
 
+def loop_message_n_times (msg, n)
+  n.times { puts msg }
+end
+
+def output_array (arr)
+  i = 0
+  while i < arr.length do
+    puts arr[i]
+    i += 1
+  end
+end
